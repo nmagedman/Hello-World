3b64ade8add2bcdf77b54cd1fdb749eb14d99984
Noach Magedman <nmagedman@gmail.com>
2011-09-09 14:51:36 +0300

    Created vanilla Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SyGen::Application.config.secret_token = '62714234b75794f4b949508d9d3a1d6133bcffa66c32268093e2bd713c3801c6f312ef3f48c468324167d108887bc03d66af0063f214526e5f3f7b727bdb4170'
